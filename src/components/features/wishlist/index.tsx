753c441314da59fb55e6463d1fe3a99b72586ffa
phareedah1 <phareedah2007@gmail.com>
2025-08-29 17:26:38 +0100

    cleanup

--- src/components/features/wishlist/index.tsx
+++ src/components/features/wishlist/index.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import { useWishlistStore } from "./wishliststore";
 import styles from "./styles.module.scss";
 import trashIcon from "../../../../public/assets/images/shopping/delete2.svg"; // use your delete icon path
+import star from '../../../../public/assets/images/shopping/star.svg'
+import Link from "next/link";
+import { useCartStore } from "@/store/cartStore";
 
 export default function Wishlist() {
   const { wishlist, removeFromWishlist, clearWishlist } = useWishlistStore();
-
+  const AddToCart = useCartStore((state) => state.AddToCart);
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>My Wishlist</h2>
@@ -16,30 +19,42 @@ export default function Wishlist() {
       ) : (
         <div className={styles.grid}>
           {wishlist.map((product) => (
-            <div key={product.id} className={styles.card}>
-              <div className={styles.imageWrapper}>
-                <Image
-                  src={product.image}
-                  alt={product.name}
-                  width={250}
-                  height={300}
-                  className={styles.image}
-                />
-                <Image
-                  src={trashIcon}
-                  alt="Remove"
-                  className={styles.deleteIcon}
-                  onClick={() => removeFromWishlist(product.id)}
-                />
-              </div>
-              <div className={styles.details}>
-                <h4 className={styles.name}>{product.name}</h4>
-                <div className={styles.priceWrapper}>
-                  <span className={styles.price}>₦{product.price}</span>
-                  <span className={styles.oldPrice}>₦{product.oldPrice}</span>
-                </div>
-              </div>
+            <div className={styles.imageWrapper}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={300}
+          height={400}
+          className={styles.image}
+        />
+        <Image
+          src={trashIcon}
+          alt="Remove"
+          className={styles.deleteIcon}
+          width={25}
+          height={25}
+          onClick={() => removeFromWishlist(product.id)}
+        />
+
+        {/* Heart Icon */}
+
+        <div className={styles.details}>
+          <h4 className={styles.name}>{product.name}</h4>
+          <div className={styles.priceWrapper}>
+            <div>
+              <span className={styles.price}>₦{product.price}</span>
+              <span className={styles.oldPrice}>₦{product.oldPrice}</span>
+              <Image src={star} alt="rating" className={styles.star}/>
             </div>
+            <Link href="/cart">
+              <div>
+                <button>Add to cart</button>
+              </div>
+            </Link>
+          </div>
+        </div>
+        </div>
+
           ))}
         </div>
       )}
